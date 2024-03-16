fdf94a51ffa639a216922871cf7e669219ccc7c2
CHENCHAIAH MEKALATHURU <137814794+iamchenchu@users.noreply.github.com>
2024-03-16 11:59:09 -0500

    Added the code

---
+++ 01DataStructures/00ds_basics.py
@@ -0,0 +1,11 @@
+# 4 data structures in python : list, tuple, dictionary, set
+
+# 1. List : ordered, mutable, allows duplicate elements
+# 2. Tuple : ordered, immutable, allows duplicate elements
+# 3. Set : unordered, mutable, no duplicate elements
+# 4. Dictionary : unordered, mutable, no duplicate elements
+
+# List
+
+list1 = [12, 32, 53, 29, 29, 10]
+print(list1)
---
+++ 02Operators/00basics_operators.py
@@ -0,0 +1,21 @@
+# Operators in Python
+# 1. Arithmetic Operators : +, -, *, /, %, //, **
+# 2. Comparison Operators : ==, !=, >, <, >=, <=
+# 3. Logical Operators : and, or, not
+# 4. Bitwise Operators : &, |, ~, ^, <<, >>
+# 5. Assignment Operators : =, +=, -=, *=, /=, %=, //=, **=
+# 6. Identity Operators : is, is not
+# 7. Membership Operators : in, not in
+# 8. Ternary Operator : [on_true] if [expression] else [on_false]
+# 9. Operator Precedence : BODMAS : Brackets, Orders, Division, Multiplication, Addition, Subtraction
+# 10. Operator Overloading : + operator is used for addition and concatenation
+# 11. Operator Functions : add, sub, mul, truediv, floordiv, mod, pow
+# 12. Special Operators :  __add__, __sub__, __mul__, __truediv__, __floordiv__, __mod__, __pow__
+
+def mult(a, b):
+  return a * b
+
+print(mult(11, 20))
+
+mult(10, 20)
+print(mult(10, 20))
