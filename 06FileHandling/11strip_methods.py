5198ea8144ae5d99fc9a2ed41edd496d1aa21474
CHENCHAIAH MEKALATHURU <137814794+iamchenchu@users.noreply.github.com>
2024-03-23 16:10:06 -0500

    Added the code for OOP's

---
+++ 06FileHandling/11strip_methods.py
@@ -0,0 +1,6 @@
+# 1. strip() : Removes leading and trailing whitespace from a string, including spaces, tabs, newlines, etc.
+# 2. lstrip() : Removes leading whitespace from a string. : at the beggining of the string
+# 3. rstrip() : Removes trailing whitespace from a string. : at the end of the string
+# 4. split() : Splits a string into a list of substrings based on a delimiter. : delimiter is space by default
+# 5. join() : Concatenates a list of strings into a single string.
+# 6. replace() : Replaces all occurrences of a substring within a string.
