2e7540eff186a29d144a7d7bbf7353d8e5bb4409
CHENCHAIAH MEKALATHURU <137814794+iamchenchu@users.noreply.github.com>
2026-01-28 18:44:41 -0600

    Added the code

--- 00Basics/01hello.py
+++ 00Basics/01hello.py
@@ -1,6 +1,8 @@
 print("Hello world")
+
 number = int(input("enter the number : "))
-print("the number entered is : ",number)
+
+print("the number entered is : ", number)
 
 thislist = ["apple", "banana", "cherry"]
 
